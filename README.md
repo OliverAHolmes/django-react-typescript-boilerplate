9444f96ff68131ad2217f04f86658ec38473c16d
Oliver Holmes <oliver.albert.holmes@gmail.com>
2024-06-16 13:47:24 +0930

    Add API with health check

---
+++ README.md
@@ -0,0 +1,1 @@
+# RGB Earth for the Web
---
+++ backend/api/api/urls.py
@@ -0,0 +1,48 @@
+"""
+URL configuration for api project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from .views import HealthCheckView
+from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django API Example",
+        default_version="v1",
+        description="API Example for Django",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("health/", HealthCheckView.as_view()),
+    path("", include(router.urls)),
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+]
